173fbc2c82ad599e6baa1354f44f7571cab439f8
pPlanello <pplanellodev@gmail.com>
2024-03-05 18:19:19 +0100

    Add correct information in timeline

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -40,37 +40,33 @@ export default function ContactPage(): JSX.Element {
 
 	return (
 		<>
-			<section>
-				<div className="flex flex-col p-20">
+			<section className="p-20">
+				<div className="flex flex-col">
 					<div className="items-center">
 						<Slider images={technologiesUsed} distancePerImage={30} />
 					</div>
 				</div>
 			</section>
 			{/* Timeline */}
-			<section className="bg-black text-white">
+			<section className="bg-black text-white p-20">
+				{/* Title */}
 				<div className="container mx-auto flex flex-col items-start md:flex-row my-1 md:my-1 overflow-y-auto h-64 scrollbar-hide">
 					<div className="flex flex-col w-full md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
 						<p className="ml-2 text-yellow-300 uppercase tracking-loose">
-							Working Process
+							Timeline
 						</p>
 						<p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">
-							Working Process of Fest
+							Experience & Studies
 						</p>
 						<p className="text-sm md:text-base text-gray-50 mb-4">
-							Here’s your guide to the tech fest 2021 process. Go through all
-							the steps to know the exact process of the fest.
+							Here’s a summary about my profesional life.
 						</p>
-						<a
-							href="#"
-							className="bg-transparent mr-auto hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
-						>
-							Explore Now
-						</a>
 					</div>
+					{/* Timeline list */}
 					<div className="ml-0 md:ml-12 lg:w-2/3 sticky">
 						<div className="container mx-auto w-full h-full">
 							<div className="relative wrap overflow-hidden p-10 h-full">
+								{/* line */}
 								<div
 									className="border-2-2 border-yellow-555 absolute h-full border"
 									style={{
@@ -87,82 +83,73 @@ export default function ContactPage(): JSX.Element {
 										borderRadius: '1%',
 									}}
 								></div>
+								{/* content */}
+								<div className="mb-8 flex justify-between items-center w-full right-timeline">
+									<div className="order-1 w-5/12"></div>
+									<div className="order-1  w-5/12 px-1 py-4 text-left">
+										<p className="mb-3 text-base text-yellow-300">
+											2022 - Actually
+										</p>
+										<h4 className="font-bold text-lg md:text-2xl">BBVA</h4>
+										<h5 className="mb-3 font-bold text-lg md:text-l text-gray-400">
+											Software developer
+										</h5>
+										<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+											I work as backend developer and DevOps developer. The main
+											technologies used is: Java, Python, AWS, Git
+										</p>
+									</div>
+								</div>
 								<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
 									<div className="order-1 w-5/12"></div>
 									<div className="order-1 w-5/12 px-1 py-4 text-right">
-										<p className="mb-3 text-base text-yellow-300">
-											1-6 May, 2021
-										</p>
-										<h4 className="mb-3 font-bold text-lg md:text-2xl">
-											Registration
+										<p className="mb-3 text-base text-yellow-300">2020-2021</p>
+										<h4 className="font-bold text-lg md:text-2xl">
+											Master's Degree
 										</h4>
+										<h5 className="mb-3 font-bold text-lg md:text-l text-gray-400">
+											Student in UPM
+										</h5>
 										<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-											Pick your favourite event(s) and register in that event by
-											filling the form corresponding to that event. Its that
-											easy :)
+											I was a student at the University Polytechnic of Madrid.
+											The master degree name is "Master's Degree in Network
+											Engineering and Telematic Services"
 										</p>
 									</div>
 								</div>
 								<div className="mb-8 flex justify-between items-center w-full right-timeline">
 									<div className="order-1 w-5/12"></div>
 									<div className="order-1  w-5/12 px-1 py-4 text-left">
-										<p className="mb-3 text-base text-yellow-300">
-											6-9 May, 2021
-										</p>
-										<h4 className="mb-3 font-bold text-lg md:text-2xl">
-											Participation
-										</h4>
+										<p className="mb-3 text-base text-yellow-300">2019-2020</p>
+										<h4 className="font-bold text-lg md:text-2xl">GMV</h4>
+										<h5 className="mb-3 font-bold text-lg md:text-l text-gray-400">
+											Fullstack developer
+										</h5>
 										<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-											Participate online. The links for your registered events
-											will be sent to you via email and whatsapp groups. Use
-											those links and show your talent.
+											I was a fullstack developer. I worked in project as
+											frontend developer using Angular as main technologies. I
+											worked in project as backend developer using Java and
+											Springboot as main technologies.
 										</p>
 									</div>
 								</div>
 								<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
 									<div className="order-1 w-5/12"></div>
 									<div className="order-1 w-5/12 px-1 py-4 text-right">
-										<p className="mb-3 text-base text-yellow-300">
-											{' '}
-											10 May, 2021
-										</p>
-										<h4 className="mb-3 font-bold text-lg md:text-2xl">
-											Result Declaration
+										<p className="mb-3 text-base text-yellow-300">2020-2021</p>
+										<h4 className="font-bold text-lg md:text-2xl">
+											University Degree
 										</h4>
+										<h5 className="mb-3 font-bold text-lg md:text-l text-gray-400">
+											Student in UPM
+										</h5>
 										<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-											The ultimate genius will be revealed by our judging panel
-											on 10th May, 2021 and the resukts will be announced on the
-											whatsapp groups and will be mailed to you.
+											I was a student at the University Polytechnic of Madrid.
+											The university degree name is "Degree in Engineering of
+											Telecommunication Technologies and Services"
 										</p>
 									</div>
 								</div>
-
-								<div className="mb-8 flex justify-between items-center w-full right-timeline">
-									<div className="order-1 w-5/12"></div>
-
-									<div className="order-1  w-5/12 px-1 py-4">
-										<p className="mb-3 text-base text-yellow-300">
-											12 May, 2021
-										</p>
-										<h4 className="mb-3 font-bold  text-lg md:text-2xl text-left">
-											Prize Distribution
-										</h4>
-										<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-											The winners will be contacted by our team for their
-											addresses and the winning goodies will be sent at their
-											addresses.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className="mx-auto -mt-36 md:-mt-36">
-								<Image
-									key="1"
-									src="/images/not-found-project.png"
-									alt="image"
-									width="44"
-									height="48"
-								/>
 							</div>
 						</div>
 					</div>
