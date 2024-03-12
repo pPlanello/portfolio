4f99ae37d22678b83e9168cb5d9775b048addb0b
pPlanello <planello96@gmail.com>
2024-03-12 19:30:22 +0100

    Add new style background in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
 			<body
 				className={`bg-main-theme dark:bg-main-theme-dark text-main-theme-dark dark:text-main-theme ${inter.className}`}
 			>
+				<div className="bg-[#d6c0a7] dark:bg-[#a16d4e] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[8rem] dark:blur-[20rem] sm:w-[68.75rem]"></div>
+				<div className="bg-[#d1cbfd] dark:bg-[#51528b] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[8rem] dark:blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 				<DarkModeProvider>
 					<Notification />
 					<Navbar />
